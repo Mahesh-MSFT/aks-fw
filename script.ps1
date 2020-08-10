e674491f4143688ebf39b3f1554289383c8e37b8
Mahesh Kshirsagar <maksh@microsoft.com>
2020-08-10 17:14:33 +0100

    added app

--- script.ps1
+++
@@ -1,7 +0,0 @@
-$SUB_ID=$(az keyvault secret show --name "subscriptionid" --vault-name "maksh-key-vault" --query value)
-$APP_ID=$(az keyvault secret show --name "clientid" --vault-name "maksh-key-vault" --query value)
-$APPSECRET=$(az keyvault secret show --name "clientsecret" --vault-name "maksh-key-vault" --query value)
-$TENANT_ID=$(az keyvault secret show --name "tenantid" --vault-name "maksh-key-vault" --query value)
-$PASSWORD=$(az keyvault secret show --name "clientsecret" --vault-name "maksh-key-vault" --query value)
-
-az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
